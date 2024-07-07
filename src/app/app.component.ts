97d567c29fee0c805ac931ac60205626d7c6a316
JakubCzuba <jakub.czuba.it@gmail.com>
2024-07-08 00:34:10 +0200

    homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloworldComponent } from './components/helloworld/helloworld.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloworldComponent],
+  imports: [RouterOutlet, HelloworldComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
