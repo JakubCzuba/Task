213f6093b04d63f933c5ee3382b78be3da1c69de
JakubCzuba <jakub.czuba.it@gmail.com>
2024-07-11 00:22:48 +0200

    my first directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { MenuComponent } from './components/menu/menu.component';
 import { WelcomebuttonComponent } from './components/welcomebutton/welcomebutton.component';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { CommonModule } from '@angular/common';
+import { HwcolorDirective } from './directives/hwcolor/hwcolor.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloworldComponent, WelcomebuttonComponent, HighlightDirective, CommonModule],
+  imports: [RouterOutlet, HelloworldComponent, WelcomebuttonComponent, HighlightDirective, CommonModule, HwcolorDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   encapsulation: ViewEncapsulation.None
