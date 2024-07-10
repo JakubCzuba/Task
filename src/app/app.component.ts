7d2e9aab4f1f280bf93da9f56df416cf29a45de1
JakubCzuba <jakub.czuba.it@gmail.com>
2024-07-10 22:53:12 +0200

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloworldComponent } from './components/helloworld/helloworld.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { WelcomebuttonComponent } from './components/welcomebutton/welcomebutton.component';
+import { HighlightDirective } from './directives/highlight/highlight.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloworldComponent, WelcomebuttonComponent],
+  imports: [RouterOutlet, HelloworldComponent, WelcomebuttonComponent, HighlightDirective, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'task';
+  x = 2;
 }
