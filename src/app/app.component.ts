265beb3458589a834649481c5377657b45cecc50
JakubCzuba <jakub.czuba.it@gmail.com>
2024-07-15 15:05:40 +0200

    field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { WelcomebuttonComponent } from './components/welcomebutton/welcomebutton
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { CommonModule } from '@angular/common';
 import { HwcolorDirective } from './directives/hwcolor/hwcolor.directive';
+import { FieldComponent } from './components/field/field.component';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,7 @@ import { HwcolorDirective } from './directives/hwcolor/hwcolor.directive';
     HighlightDirective, 
     CommonModule, 
     HwcolorDirective, 
+    FieldComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
